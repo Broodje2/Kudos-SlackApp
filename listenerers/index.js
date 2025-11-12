6a4b5dcdc3c89205e22d48f6344a596124f3767e
Nohlmans,Guus G. <guus.nohlmans@gmail.com>
2025-11-12 11:50:01 +0100

    Removed unused commands

--- listenerers/index.js
+++ listenerers/index.js
@@ -1,13 +1,13 @@
-// import giveKudos from "./giveKudos.js";
-import {getUser, registerAccount, sync, autoSync, userChanged } from "./user.js"
+// import {getUser, registerAccount, sync, autoSync, userChanged } from "./user.js"
+import {autoSync, userChanged } from "./user.js"
 import { kudosRecommendation, giveKudos, viewKudosModal, checkGiveawayKudos } from "./kudos.js"
 import { leaderboard } from "./leaderboard.js";
 
 export default function registerListeners(app) {
   //user
-  getUser(app);
-  registerAccount(app);
-  sync(app);
+  // getUser(app);
+  // registerAccount(app);
+  // sync(app);
   autoSync(app);
   userChanged(app);
 
